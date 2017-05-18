8d12719dedf5610dffb08e5c557ef5a76cc314a1
fm20115412 <641290900@qq.com>
2017-05-18 18:40:12 +0800

    webpack demo

---
+++ task1/app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by fm on 2017/5/18.
+ */
+import bar from './bar';
+
+bar();
---
+++ task1/bar.js
@@ -0,0 +1,6 @@
+/**
+ * Created by fm on 2017/5/18.
+ */
+export default function bar() {
+    alert("hello webpack")
+}
