5774701ad9805f5f4383b56f0d1542462cea6e6d
fm20115412 <641290900@qq.com>
2017-06-03 15:11:03 +0800

    fix data struct

--- resumer/src/store/index.js
+++ resumer/src/store/index.js
@@ -14,37 +14,30 @@ export default new Vuex.Store({
     },
     resume:{
       config:[
-        {field:"profile",icon:"id"},
-        {field:"workHistory",icon:"work"},
-        {field:"education",icon:"book"},
-        {field:"projects",icon:"heart"},
-        {field:"awards",icon:"cup"},
-        {field:"contacts",icon:"phone"},
+        {field:"profile",icon:"id",keys:["name","city","title","birthday"]},
+        {field:"workHistory",icon:"work",keys:["company","details"]},
+        {field:"education",icon:"book",keys:["school","details"]},
+        {field:"projects",icon:"heart",keys:["name","details"]},
+        {field:"awards",icon:"cup",keys:["name","details"]},
+        {field:"contacts",icon:"phone",keys:["contact","content"]},
       ],
       profile:{
-        name:"babybear",
-        city:"重庆",
-        title:"研究生"
+
       },
-      'workHistory': [
-        {company: 'AL', content: '我的第二份工作是'},
-        {company: 'TX', content: '我的第一份工作是'},
+      workHistory: [
+
       ],
       education: [
-        {school: '重庆大学', content: '本科'},
-        {school: '重庆大学', content: '研究生'},
+
       ],
       projects: [
-        {name: 'project A', content: '文字'},
-        {name: 'project B', content: '文字'},
+
       ],
       awards: [
-        {name: 'awards A', content: '文字'},
-        {name: 'awards B', content: '文字'},
+
       ],
       contacts: [
-        {contact: 'phone', content: '13812345678'},
-        {contact: 'qq', content: '12345678'},
+
       ]
 },
   },
